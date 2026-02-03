88f4dcb6fb59aac612a69ac98dec5038d42cd342
Sreesanth-01 <iamshanth7@gmail.com>
2026-02-03 10:40:10 +0530

    Initial setup

---
+++ src/main/java/com/project/mentornet/MentornetApplication.java
@@ -0,0 +1,13 @@
+package com.project.mentornet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentornetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentornetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mentornet/MentornetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mentornet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentornetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
